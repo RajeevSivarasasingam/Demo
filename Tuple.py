544ce10f5797bc2c500c67c9b2e037e39f605307
RajeevSivarasasingam <sivarajeev511@gmail.com>
2026-02-18 00:25:32 +0530

    create tuple as my_tuple

--- Tuple.py
+++ Tuple.py
@@ -1,1 +1,5 @@
 #Let's create a tuple of 3 elements
+my_tuple = ("apple", "banana", "cherry")
+print(my_tuple)
+
+# Tuples are immutable, which means you cannot change their values after they are created. However, you can create a new tuple by concatenating existing tuples.
