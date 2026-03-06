32a2ad98c11c63351dff03894ad91a749819574e
Rajeev <sivarajeev511@gmail.com>
2026-03-06 22:17:19 +0530

    add new file set

--- Tuple.py
+++ Tuple.py
@@ -1,6 +1,16 @@
+#Tuples are used to store multiple items in a single variable.
+#4 built-in data types in Python used to store collections of data,
+# tuples, lists, dictionaries, and sets.
+
+
 #Let's create a tuple of 3 elements
 my_tuple = ("apple", "banana", "cherry")
 print(my_tuple)
+
+
+# it allows duplicate values
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")    #here we have duplicate values "apple" and "cherry"
+print(thistuple)
 
 # Tuples are immutable, which means you cannot change their values after they are created. However, you can create a new tuple by concatenating existing tuples.
 # Concatenating tuples
---
+++ set.py
@@ -0,0 +1,10 @@
+
+
+# Sets are used to store multiple items in a single variable.
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+
+# Sets are unordered, unchangeable, and do not allow duplicate values.
+thisset = {"apple", "banana", "cherry", "apple"}    #here we have duplicate value "apple"
+print(thisset)
