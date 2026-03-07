d0d8039dd5523d3302292707fc3bf4ae5ec10b0d
Rajeev <sivarajeev511@gmail.com>
2026-03-07 22:15:07 +0530

    edit and update set.pyt file

--- set.py
+++ set.py
@@ -8,3 +8,12 @@ print(thisset)
 # Sets are unordered, unchangeable, and do not allow duplicate values.
 thisset = {"apple", "banana", "cherry", "apple"}    #here we have duplicate value "apple"
 print(thisset)
+
+
+#True and 1 is considered the same value:
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+print(thisset)
+
+#can store different data types in a set
+set1 = {"abc", 34, True, 40, "male"}
+
