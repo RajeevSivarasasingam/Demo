eb9df3d05b45a83d460c068f5e0d070620bd0875
Rajeev <sivarajeev511@gmail.com>
2026-03-20 23:55:39 +0530

    update funtion file

--- funtion.py
+++ funtion.py
@@ -3,3 +3,11 @@ def my_Function():
   print("Hello World")
 
   my_Function()
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
