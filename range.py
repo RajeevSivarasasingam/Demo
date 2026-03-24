b4388a31d2de53d8124c23001d9d9b6e19dc9370
Rajeev <sivarajeev511@gmail.com>
2026-03-24 23:14:55 +0530

    update range file

---
+++ range.py
@@ -0,0 +1,8 @@
+
+x = range(3, 10)
+print(x) # this will print the range object
+print(list(x)) # this will print the list of numbers in the range
+
+y=range(0, 20, 5) # this will create a range from 0 to 20 with a step of 5
+print(list(y)) # this will print the list of numbers in the range
+
