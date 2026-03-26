c1fbcb8a1ae3ecff45d4c6ed1c9af55429b09dcd
Rajeev <sivarajeev511@gmail.com>
2026-03-26 23:01:39 +0530

    create array.py file

--- range.py
+++ range.py
@@ -6,3 +6,7 @@ print(list(x)) # this will print the list of numbers in the range
 y=range(0, 20, 5) # this will create a range from 0 to 20 with a step of 5
 print(list(y)) # this will print the list of numbers in the range
 
+print(list(range(5)))
+print(list(range(1, 6)))
+print(list(range(5, 20, 3)))
+
