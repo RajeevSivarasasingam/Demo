e754ada4dd6053e23d5fb73117f9b0b748dd0ccb
Rajeev <sivarajeev511@gmail.com>
2026-03-29 19:48:43 +0530

    update iterator file

--- iterators.py
+++ iterators.py
@@ -5,3 +5,10 @@ myit = iter(mytuple)
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+tuple1=("cat", "dog", "rabbit")
+for x in tuple1:
+  print(x)
+mystr = "banana"
+for x in mystr:
+  print(x)
