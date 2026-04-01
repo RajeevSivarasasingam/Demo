e35a91cab1fdc2f8947d2317362a29931e14a556
Rajeev <sivarajeev511@gmail.com>
2026-04-01 23:32:03 +0530

    update userinput

--- userInput.py
+++ userInput.py
@@ -6,3 +6,7 @@ print("Hello, " + name)
 print("type your age: ")
 age = input()
 print("You are " + age + " years old.") 
+
+print("type your favorite color: ")
+color = input() 
+print("Your favorite color is " + color)
