4da91da7c1bce7afbf5761ab182ce91662fb7b8e
Rajeev <sivarajeev511@gmail.com>
2026-09-19 20:45:10 +0530

    update the file

--- loop.py
+++ loop.py
@@ -1,1 +1,31 @@
 print("I'm Rajeev")
+
+#while loop,for loop,break,continue,pass
+
+#1.while loop
+i=1
+while i <= 10:
+    print(i)
+    i += 1
+
+#2.for loop
+for i in range(1, 11):
+    print(i)
+
+#3.break statement
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+
+#4.continue statement
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+
+#5.pass statement
+for i in range(1, 11):
+    if i == 5:
+        pass
+    print(i)
