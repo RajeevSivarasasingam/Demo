4d680b37f4376b1d3e6585116a0be4c844984600
Rajeev <sivarajeev511@gmail.com>
2026-09-20 20:51:14 +0530

    update file

--- loop.py
+++ loop.py
@@ -29,3 +29,13 @@ for i in range(1, 11):
     if i == 5:
         pass
     print(i)
+
+ #pattern printing
+ # get input and  print number in    , first row 1, second row 2, third row 3, fourth row 4, fifth row 5
+
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ") #end is used to print in the same line
+    print()
+
